76dc35bc8d13f2b949e48d1c228bb0cde7b68a33
shoffkarim <k4r1mch1k@gmail.com>
2022-09-29 11:16:42 +0500

    create header

---
+++ src/components/header/header.tsx
@@ -0,0 +1,38 @@
+import React from 'react'
+import { Menu } from '@mui/icons-material'
+import { AppBar, IconButton, styled, Toolbar, Typography } from '@mui/material'
+
+
+const StyledToolbar = styled(Toolbar)(({ theme }) => ({
+    display: 'flex',
+    alignItems: 'center',
+    paddingTop: theme.spacing(1),
+    paddingBottom: theme.spacing(1),
+  }));
+
+export const Header = () => {
+  return (
+    <AppBar color="primary" enableColorOnDark>
+        <StyledToolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="open drawer"
+            sx={{ mr: 2 }}
+          >
+            <Menu />
+          </IconButton>
+          <Typography
+            variant="h6"
+            noWrap
+            component="h6"
+          >
+            Klearning
+          </Typography>
+        </StyledToolbar>
+    </AppBar>
+  )
+}
+
+export default Header
